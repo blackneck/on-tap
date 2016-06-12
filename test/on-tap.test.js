bca639d06bdf91a3e356ea247abb6c737b7bc86d
Clement Allen <clementallen313@gmail.com>
2016-06-12 18:01:26 +0100

    added more tests

--- test/on-tap.test.js
+++ test/on-tap.test.js
@@ -28,6 +28,12 @@ describe('At Location', () => {
         });
     });
 
+    it('should return invalid bar error if no bar name is passed', () => {
+        onTap.atLocation(null, (err, beers) => {
+            assert.equal(err, 'Invalid bar', 'correct error returned');
+        });
+    });
+
     it('should return site unavailable error if site not found', () => {
         mockResponse.statusCode = 404;
         sandbox.stub(needle, 'get').yields(null, mockResponse);
@@ -47,7 +53,25 @@ describe('At Location', () => {
         });
     });
 
-    it('should return filtered list of beers on tap', () => {
+    it('should return expected uk bar url', () => {
+        mockResponse.statusCode = 200;
+        sandbox.stub(needle, 'get').yields(null, mockResponse);
+
+        onTap.atLocation('aberdeen', (err, beers) => {
+            assert.equal(needle.get.firstCall.args[0], 'https://www.brewdog.com/bars/uk/aberdeen');
+        });
+    });
+
+    it('should return expected worldwide bar url', () => {
+        mockResponse.statusCode = 200;
+        sandbox.stub(needle, 'get').yields(null, mockResponse);
+
+        onTap.atLocation('brussels', (err, beers) => {
+            assert.equal(needle.get.firstCall.args[0], 'https://www.brewdog.com/bars/worldwide/brussels');
+        });
+    });
+
+    it('should return expected list of beers on tap', () => {
         mockResponse.statusCode = 200;
         mockResponse.body = '<div class="barText">BREWDOG HEADLINERS<br>Dead Pony Club 3.8%<br>Jet Black Heart 4.7%<br>Kingpin lager 4.7%<br>5AM Saint 5.0%<br>Punk IPA 5.4%<br>OTHER BREWDOG DRAFT<br>Vagabond Pale Ale (GF) 4.5%<br>Bourbon Baby 5.9%<br>--- Extra Beers ---<br>Elvis Juice 6.5%<br>DIPA 9.2%<br>AB:20 14.1%<br>GUEST DRAFT<br>Summer Love 5.2% Victory<br>Fruitlands 4.8% Modern Times<br>Monster\'s Park 12% Modern Times<br>Lomaland Saison 5.5% Modern Times<br>Gueuze 100% Lambic Bio 5% Cantillon<br>Framboise 2.5% Lindemans<br></div>';
         sandbox.stub(needle, 'get').yields(null, mockResponse);
